8823ece8bd9eeda05ff173241add8436dd3d6b15
BeatTheCode <35395996+BeatTheCode@users.noreply.github.com>
2018-01-13 11:16:46 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1; i<counter(13);i++) {
+			System.out.println("Hello World!");
+			System.out.println(counter(i));
+		}
+	}
+
+
+
+	public static int counter (int count) {
+		return(count*count);
+	}
+
+}
